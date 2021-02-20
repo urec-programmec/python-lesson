bd714fdfa1a6ed125cf8ae874d7e3ad3e3c6899a
Ruslan Sergeev <inst.r.s@bk.ru>
2021-02-20 14:38:08 +0300

    Работа с FOR , НАЧАЛЬНЫЕ ФУНКЦИИ

--- LR0/main.py
+++ LR0/main.py
@@ -1,2 +1,12 @@
-print("Hello world")
-print("AAAAAAAAAAAAAAAAA")
+from function import function
+import matplotlib.pyplot as plt
+for i in range(10):
+    print("i")
+xs = [1, 2, 3, 4, 5]
+ys = []
+for i in xs:
+    ys.append(function(i))
+print(xs)
+print(ys)
+plt.plot (xs,ys)
+plt.show()
